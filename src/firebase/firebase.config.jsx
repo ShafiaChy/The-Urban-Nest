972bfe77c9f734ceefa86cc7b3d6cd8fc6e2ceab
unknown <shafiachy04@gmail.com>
2024-11-24 01:41:53 +0600

    firebase config added

--- src/firebase/firebase.config.jsx
+++ src/firebase/firebase.config.jsx
@@ -1,13 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOoSsffaWccTaq75OjBaMqnb02voaKxAA",
-  authDomain: "b-7-mi-12.firebaseapp.com",
-  projectId: "b-7-mi-12",
-  storageBucket: "b-7-mi-12.appspot.com",
-  messagingSenderId: "72868017447",
-  appId: "1:72868017447:web:24b483ed411a72862de502",
-  measurementId: "G-3S32RHWGHH"
+  apiKey: "AIzaSyDVfobQehwGPsGibfHEcQIQjrGdIvu30JE",
+  authDomain: "bistro-boss-e5090.firebaseapp.com",
+  projectId: "bistro-boss-e5090",
+  storageBucket: "bistro-boss-e5090.firebasestorage.app",
+  messagingSenderId: "891451164848",
+  appId: "1:891451164848:web:a3ebad909fbac3cbd0d375"
 
   // apiKey: import.meta.env.VITE_API_KEY,
   // authDomain: import.meta.env.VITE_AUTH_DOMAIN,
